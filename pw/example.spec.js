8a2912de2464470fbf484e7d16aea08b4a782bbf
Murat Ozcan <muratkerem@gmail.com>
2024-11-24 06:18:44 -0600

    a done

--- pw/example.spec.js
+++ pw/example.spec.js
@@ -1,13 +1,28 @@
 // @ts-check
 const { test, expect } = require('@playwright/test')
 
-test('has title', async ({ page }) => {
-  await page.goto('/')
+test.beforeEach(async ({ request, page }) => {
+  await request.post('/reset', { data: { todos: [] } })
 
+  // if the application throws an unhandled error
+  // we want to fail the test. Make sure to register
+  // the error callback before visiting the page
+  page.on('pageerror', (exception) => {
+    throw new Error('App threw an error')
+  })
+
+  await page.goto('/')
+})
+
+test('has title', async ({ page }, testInfo) => {
+  console.log('running test "%s"', testInfo.titlePath.join('/'))
   await expect(page).toHaveTitle('cy-vs-pw-example-todomvc')
 
   // https://playwright.dev/docs/locators
-  // and the count assertion
   // https://playwright.dev/docs/api/class-locatorassertions
-  await expect(page.locator('.todo-list li')).toHaveCount(3, { timeout: 5000 })
+  await expect(page.locator('body')).toHaveClass('loaded')
+  // same thing
+  await page.locator('body.loaded').waitFor()
+
+  await expect(page.locator('.todo-list li')).toHaveCount(0, { timeout: 1000 })
 })
