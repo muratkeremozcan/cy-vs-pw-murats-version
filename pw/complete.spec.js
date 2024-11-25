d99bcd7c8ba9f17f47170f4156f0682375b168fd
Murat Ozcan <muratkerem@gmail.com>
2024-11-25 07:05:22 -0600

    feat: b4

--- pw/complete.spec.js
+++ pw/complete.spec.js
@@ -1,33 +1,25 @@
 // @ts-check
 const { test, expect } = require('@playwright/test')
+const items = require('../fixtures/three.json')
 
 test.describe('Complete todos', () => {
   test.beforeEach(async ({ request, page }) => {
-    await request.post('/reset', { data: { todos: [] } })
+    await request.post('/reset', { data: { todos: items } })
     await page.goto('/')
     await page.locator('body.loaded').waitFor()
   })
 
   test('completes a todo', async ({ page }) => {
-    const input = page.getByPlaceholder('What needs to be done?')
     const todos = page.locator('.todo-list li')
     const todoLabels = todos.locator('label')
     const count = page.locator('[data-cy="remaining-count"]')
 
-    await input.fill('Write code')
-    await input.press('Enter')
-    await input.fill('Write tests')
-    await input.press('Enter')
-    await input.fill('Make tests pass')
-    await input.press('Enter')
+    const labels = items.map((item) => item.title)
 
-    await expect(todoLabels).toHaveText([
-      'Write code',
-      'Write tests',
-      'Make tests pass',
-    ])
+    await expect(todoLabels).toHaveCount(items.length)
+    await expect(todoLabels).toHaveText(labels)
 
-    await expect(count).toHaveText('3')
+    await expect(count).toHaveText(String(items.length))
 
     await todos.first().locator('.toggle').click()
     await expect(todos.first()).toHaveClass(/completed/)
@@ -37,7 +29,7 @@ test.describe('Complete todos', () => {
 
     await expect(todos).toHaveClass(['todo completed', 'todo', 'todo'])
 
-    await expect(count).toHaveText('2')
+    await expect(count).toHaveText(String(items.length - 1))
 
     await expect(
       page.getByRole('button', { name: 'Clear completed' }),
