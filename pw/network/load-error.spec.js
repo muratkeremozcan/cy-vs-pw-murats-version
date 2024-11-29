b6043e13b5926475e9bf7e1e1def19e38770f8fa
Murat Ozcan <muratkerem@gmail.com>
2024-11-29 04:10:35 -0600

    d

---
+++ pw/network/load-error.spec.js
@@ -0,0 +1,74 @@
+// @ts-check
+
+const { test, expect } = require('@playwright/test')
+const { interceptNetworkCall } = require('../utils/network')
+
+test.describe('App', () => {
+  test('logs a server error', async ({ page }) => {
+    // stub the "GET /todos" route and return
+    // an object with status code 500
+    // and the body "server error"
+    // spy on the "GET /todos" resource
+    await page.route('/todos', (route) =>
+      route.fulfill({
+        status: 500,
+        body: 'server error',
+      }),
+    )
+    const load = page.waitForResponse('/todos')
+
+    // set up an array to collect all text the app
+    // prints using "console.error()" method
+    // https://playwright.dev/docs/api/class-consolemessage
+    const errorMessages = []
+    page.on('console', (msg) => {
+      if (msg.type() === 'error') {
+        errorMessages.push(msg.text())
+      }
+    })
+
+    // visit the page
+    await page.goto('/')
+    // and wait for the "GET /todos" call
+    await load
+
+    // retry checking the error messages array
+    // to find the message "server error"
+    // that the application should print when it receives
+    // an error response from the backend
+    await expect(() =>
+      expect(errorMessages, 'error message').toContain('server error'),
+    ).toPass()
+  })
+
+  test('network helpers version -logs a server error', async ({ page }) => {
+    // Stub the "GET /todos" route and return an object with status code 500
+    await interceptNetworkCall({
+      method: 'GET',
+      url: '/todos',
+      page,
+      fulfillResponse: {
+        status: 500,
+        body: 'server error',
+        headers: {
+          'Content-Type': 'text/plain',
+        },
+      },
+    })
+
+    const errorMessages = []
+    page.on('console', (msg) => {
+      if (msg.type() === 'error') {
+        errorMessages.push(msg.text())
+      }
+    })
+
+    // Visit the page
+    await page.goto('/')
+
+    // Wait for the error message to be logged
+    await expect
+      .poll(() => errorMessages, { message: 'Waiting for error message' })
+      .toContain('server error')
+  })
+})
--- pw/network/todos-delete.spec.js
+++ pw/network/todos-delete.spec.js
@@ -1,5 +1,6 @@
 // @ts-check
 const { test, expect } = require('@playwright/test')
+const { interceptNetworkCall } = require('../utils/network')
 
 test.describe('App', () => {
   test.beforeEach(async ({ page }) => {
@@ -43,6 +44,48 @@ test.describe('App', () => {
     const deleteResponse = await deleteRequest.response()
     const deleteResponseStatus = await deleteResponse?.status()
     expect(deleteResponseStatus).toBe(200)
+
+    // confirm the new todo is no longer on the page
+    await expect(todoElement).not.toBeAttached()
+  })
+
+  test('network helpers version - deletes a todo', async ({ page }) => {
+    const title = 'Advance ' + Math.round(Math.random() * 1e6)
+
+    // spy on the "POST /todos" network call
+    const postTodo = interceptNetworkCall({
+      method: 'POST',
+      url: '/todos',
+      page,
+    })
+
+    // enter the new todo with a random title
+    await page.locator('input.new-todo').fill(title)
+    await page.locator('input.new-todo').press('Enter')
+
+    // wait for the post call and get the todo item id
+    const {
+      data: { id },
+    } = await postTodo
+
+    // spy on the "DELETE /todos/:id" network call
+    const deleteTodo = interceptNetworkCall({
+      method: 'DELETE',
+      url: `/todos/${id}`,
+      page,
+    })
+
+    // find the newly entered todo item
+    // and click on the delete button
+    const todoElement = await page.locator('li.todo', { hasText: title })
+    await todoElement.hover()
+    await todoElement.locator('.destroy').click()
+
+    // confirm the delete network call happens
+    // and the server responded with the status code 200
+    /** @type {{ status: number }} */
+    const { status } = await deleteTodo
+    expect(status).toBe(200)
 
     // confirm the new todo is no longer on the page
     await expect(todoElement).not.toBeAttached()
